0ff424e56048df8126f8db2bf423fe4521c2c907
kickbuttowski80 <pazpaz25@gmail.com>
2020-04-10 19:43:24 -0700

    I setuped custome themes inside vuetify.js plugin

--- src/components/DrawerMenu.vue
+++ src/components/DrawerMenu.vue
@@ -32,6 +32,7 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
+
   </v-app>
 </template>
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,9 +4,9 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    // icons:{
-    //     iconfont: 'mdi',
-    // },
+    icons:{
+        iconfont: 'mdi',
+    },
     theme:{
         themes:{
             light:{
