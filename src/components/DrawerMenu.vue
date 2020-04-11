0acc53a2b377c533b318dac85033ff9a445f0d3e
kickbuttowski80 <pazpaz25@gmail.com>
2020-04-10 19:42:24 -0700

    setup vuetify, and used baseline pre-made layouts and put it in different component and used it in App.vue, yet still contact phone icon does not show up

---
+++ src/components/DrawerMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-phone</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+      name: "DrawerMenu",
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,24 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    // icons:{
+    //     iconfont: 'mdi',
+    // },
+    theme:{
+        themes:{
+            light:{
+                primary: '#333333',
+                secondary: '#2B2B2B',
+                white: '#ffffff',
+                orange: '#FC6D2B',
+                darkgrey: '#707070',
+                complete: '#2BFC83',
+                inprogress: '#FF8936',
+                incomplete: '#FF1212',
+                lightgrey: '#cacaca'
+
+            }
+        }
+    }
+
 });
