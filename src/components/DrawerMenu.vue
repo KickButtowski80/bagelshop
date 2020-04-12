7f8f88e201001bac8237abea9b99f14d5a37b8ea
kickbuttowski80 <pazpaz25@gmail.com>
2020-04-11 20:58:44 -0700

    cleaned the DrawerMenu component

--- src/components/DrawerMenu.vue
+++ src/components/DrawerMenu.vue
@@ -1,13 +1,10 @@
 <template>
-  <v-app id="app">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon> home</v-icon>
+            <v-icon>home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title>Home</v-list-item-title>
@@ -15,7 +12,7 @@
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon> contact_mail</v-icon>
+            <v-icon>contact_mail</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title>Contact</v-list-item-title>
@@ -24,31 +21,21 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      color="orange"
-      dark
-    >
+    <v-app-bar app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
-    <v-content>
-      <v-container class="fill-height" fluid>
-          
-
-      </v-container>
-    </v-content>
-  </v-app>
+  </div>
 </template>
 
 <script>
-  export default {
-      name: "DrawerMenu",
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-    }),
-  }
+export default {
+  name: "DrawerMenu",
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
 </script>
