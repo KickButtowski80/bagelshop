199bdc8eb2e20d67b94cd39501c94ffc48b89018
kickbuttowski80 <pazpaz25@gmail.com>
2020-04-14 21:16:24 -0700

    start styling the menu component by having two rows and each rows has a col with a div inside it

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>Menu</div>
+  <v-container>
+    <v-row>
+      <v-col md="5" offset-md="1">
+        <div class="pa-2 info">Left</div>
+      </v-col>
+      <v-col md="4" offset-md="1">
+        <div class="pa-2 info">Right</div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: 'Menu'
+  name: "Menu"
 };
 </script>
+
+<style lang="scss" scoped>
+.info {
+  border: 1px black solid;
+}
+</style>
