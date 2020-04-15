501307a7ae15f49c10dfd96d906fd671ab72beac
kickbuttowski80 <pazpaz25@gmail.com>
2020-04-14 21:31:23 -0700

    created two compoenets leftTableMenu and rightTableMenu which each of them has a table with some randome content and corporated them inside the menu componenet

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,18 +2,28 @@
   <v-container>
     <v-row>
       <v-col md="5" offset-md="1">
-        <div class="pa-2 info">Left</div>
+        <div class="pa-2 info">
+            <left-table></left-table>
+        </div>
       </v-col>
       <v-col md="4" offset-md="1">
-        <div class="pa-2 info">Right</div>
+        <div class="pa-2 info">
+            <right-table></right-table>
+        </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import leftTabelMenu from '../components/leftTableMenu' 
+import rightTableMenuVue from '../components/rightTableMenu.vue';
 export default {
-  name: "Menu"
+  name: "Menu",
+  components:{
+      'left-table': leftTabelMenu,
+      'right-table' : rightTableMenuVue
+  }
 };
 </script>
 
