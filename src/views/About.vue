0efcc4edfe4d3acbb6614630eb4fb22f7dc37a4b
kickbuttowski80 <pazpaz25@gmail.com>
2020-04-14 19:33:09 -0700

    styled about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container grid-list-xs>
+    <v-row align-self="center" class="info_box">
+      <v-col sm="9" xs="12" offset-sm="2">
+        <h1>About IT Bagel Shop</h1>
+        <h2>All you need to know Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quas ipsum non veniam totam porro aspernatur natus excepturi facilis a, recusandae aut nesciunt necessitatibus mollitia deleniti asperiores architecto quaerat. Dolor!</h2>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
