0e6db9fea3dfac3a375502ab187c62c79291ce63
kickbuttowski80 <pazpaz25@gmail.com>
2020-04-15 19:30:59 -0700

    added a simple table with 3 cols and styled them

--- src/components/leftTableMenu.vue
+++ src/components/leftTableMenu.vue
@@ -1,75 +1,69 @@
 <template>
-<div>
-  <h1 align="center">{{title}}</h1>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">Name</th>
-          <th class="text-left">Calories</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in desserts" :key="item.name">
-          <td>{{ item.name }}</td>
-          <td>{{ item.calories }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
-
-
-</div>
-
+  <div>
+    <h1 align="center">{{title}}</h1>
+    <v-simple-table od="menu-table">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Price</th>
+            <th class="text-left">Add</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in menuItems" :key="item.name">
+            <td>
+              <span id="id_name">{{item.name}}</span>
+              <br />
+              <span id="menu_item_description">{{item.description}}</span>
+            </td>
+            <td>{{ item.price }}</td>
+            <td>
+              <v-btn text>
+                <v-icon color="orange">add_shopping_cart</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-          title: "Left Menu",
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
-      }
-    },
+export default {
+  data() {
+    return {
+      title: "Left Menu",
+      menuItems: [
+        {
+          name: "Frozen Yogurt",
+          description: "just simple yougurt for your happiness",
+          price: 2
+        },
+        {
+          name: "Ice cream sandwich",
+          description: "lesser suger more happniess inside your icecream",
+          price: 2.37
+        }
+      ]
+    };
   }
+};
 </script>
+<style lang="scss" scoped>
+tr th {
+  font-weight: 300;
+}
+#id_name {
+  font-weight: bold;
+}
+tr td {
+  padding: 10px 10px 16px;
+}
+#menu_item_description {
+  font-style: italic;
+  font-weight: 300;
+  color: map-get($colors, darkgrey);
+  font-size: 13px;
+}
+</style>
