df63fbfaf1a73ff4e363cb1457906bda72f2fa15
kickbuttowski80 <pazpaz25@gmail.com>
2020-05-01 18:49:57 -0700

    changed coloring of the icons inside the menu and change the color of title inside the menu

--- src/components/DrawerMenu.vue
+++ src/components/DrawerMenu.vue
@@ -2,12 +2,12 @@
   <div>
     <v-navigation-drawer color="primary" v-model="drawer" app>
       <v-list dense>
-        <v-subheader class="display-1">Welcome</v-subheader>
+        <v-subheader  class="happy--text display-1">Welcome</v-subheader>
         <v-list-item-group>
           <v-list-item v-for="(item, i) in items" :key="i" link>
             <router-link tag="span" :to="item.link">
               <v-list-item-icon>
-                <v-icon v-text="item.icon"></v-icon>
+                <v-icon :color="item.color" v-text="item.icon"></v-icon>
               </v-list-item-icon>
             </router-link>
             <router-link tag="span" :to="item.link">
@@ -33,27 +33,28 @@
 export default {
   name: "DrawerMenu",
   props: {
-    source: String
+    source: String,
   },
   data: () => ({
     drawer: null,
     items: [
-      { text: "Home", icon: "home", link: "/" },
-      { text: "Menu", icon: "menu_book", link: "/menu" },
-      { text: "About", icon: "info", link: "/about" },
-      { text: 'Admin', icon: 'lock', link: '/admin'},
-      { text: 'Orders', icon: 'assignment', link: '/orders'}
-    ]
-  })
+      { text: "Home", icon: "home", link: "/" , color: "incomplete"},
+      { text: "Menu", icon: "menu_book", link: "/menu" , color: "incomplete"},
+      { text: "About", icon: "info", link: "/about" , color: "inprogress"},
+      { text: "Admin", icon: "lock", link: "/admin" , color: "inprogress"},
+      { text: "Orders", icon: "assignment", link: "/orders", color: "complete" },
+    ],
+  }),
 };
 </script>
 <style lang="scss" scoped>
 .v-list--dense .v-subheader {
   padding: 55px 49px;
 }
-.theme--light .v-icon {
-  color: orange !important;
-}
+// .theme--light .v-icon {
+//   color: orange !important;
+// }
+
 .theme--light.v-list-item:not(.v-list-item--active):not(.v-list-item--disabled) {
   color: orange !important;
 }
